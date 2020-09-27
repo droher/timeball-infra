7ae77a31ddd161420bc778ebf92489f628767c54
David Roher <droher@users.noreply.github.com>
2020-09-27 13:59:23 -0400

    s3 resource

--- backblaze.tf
+++ backblaze.tf
@@ -15,6 +15,12 @@ provider "backblaze" {
   }
 }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
