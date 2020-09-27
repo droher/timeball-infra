01fa66cfc30b5c920f48b2da2d7fb0104be921ec
David Roher <droher@users.noreply.github.com>
2020-09-27 11:55:34 -0400

    Create backblaze.tf

---
+++ backblaze.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    backblaze = {
+      source  = "hashicorp/aws"
+      version = ">= 3.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.backblaze_region
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+}
