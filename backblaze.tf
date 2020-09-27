f13adc10e7935b51fb9f60086c48756204757332
David Roher <droher@users.noreply.github.com>
2020-09-27 12:07:52 -0400

    Update backblaze.tf

--- backblaze.tf
+++ backblaze.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    backblaze = {
-      source  = "hashicorp/aws"
-      version = ">= 3.8.0"
-    }
-  }
-}
-
-provider "backblaze" {
+provider "aws" {
+  alias = "backblaze"
   region = "us-west-002"
 }
 
